5c073fb11f65e0bf7e555de28a08fece9e5cac35
davidyu37 <davidyu37@gmail.com>
2015-08-17 23:07:22 +0800

    courts completed

---
+++ client/app/courts/courts.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('keepballin')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('courts', {
+        url: '/courts',
+        templateUrl: 'app/courts/courts.html',
+        controller: 'CourtsCtrl'
+      });
+  });
